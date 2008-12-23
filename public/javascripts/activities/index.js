240e5992024f097755f3b970cc8d07465951e9c5
Joakim Bodin <joakim.bodin@gmail.com>
2008-12-23 20:09:02 +0100

    Fix App name inline edit

---
+++ public/javascripts/activities/index.js
@@ -0,0 +1,15 @@
+function bind_buttons() {
+  // Edit App name
+  jQuery.brazil.form.inline({
+    show_form: '.edit_app_button',
+    form_container: '.head',
+    done: function(){
+      bind_buttons();
+    }
+  });
+}
+
+$(document).ready(function() {
+  jQuery.brazil.move.scrollable('#activity_forms');
+  bind_buttons();
+});
