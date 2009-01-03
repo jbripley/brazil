dd14c9a810c7ff20bd0125ddd40c322555b80a00
Joakim Bodin <joakim.bodin@gmail.com>
2009-01-03 05:07:05 +0100

    Correctly mock new Change model

--- spec/views/activities/show_spec.rb
+++ spec/views/activities/show_spec.rb
@@ -11,13 +11,15 @@ describe "/activities/show.html.erb" do
     @activity = activities(:app_1_dev)
     @activity.should_receive(:app).at_least(:once).and_return(@app)
     
-    @change = mock(Change)
+    @change = mock_model(Change)
+    @change.stub!(:id).and_return(nil)
+    @change.stub!(:new_record?).and_return(true)
     @change.stub!(:sql).and_return('')
     @change.stub!(:dba).and_return('')
     @change.stub!(:developer).and_return('')
     @change.stub!(:activity).and_return(@activity)
     
-    errors = mock(errors)
+    errors = mock(Hash)
     errors.stub!(:count).and_return(0)
     @change.stub!(:errors).and_return(errors)
     
