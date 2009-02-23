495beb255bcdba6b878a8facf150129a71646e72
Joakim Bodin <joakim.bodin@gmail.com>
2009-02-23 10:03:48 +0100

    Require that schema be set on activity.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,27 +2,27 @@ class Activity < ActiveRecord::Base
   STATE_DEVELOPMENT = 'development'
   STATE_VERSIONED = 'versioned'
   STATE_DEPLOYED = 'deployed'
-  
+
   belongs_to :app
   has_many :versions
   has_many :changes, :order => "created_at DESC"
-  
+
   has_many :db_instance_activity
   has_many :db_instances, :through => :db_instance_activity
-  
+
   validates_associated :db_instances
-  validates_presence_of :name
+  validates_presence_of :name, :schema
 
   named_scope :latest, lambda { |limit| {:order => 'updated_at DESC', :limit => limit} }
-  
+
   def development?
     (state == Activity::STATE_DEVELOPMENT)
   end
-  
+
   def versioned?
     (state == Activity::STATE_VERSIONED)
   end
-   
+
   def versioned!
     update_attribute(:state, STATE_VERSIONED)
   end
@@ -30,7 +30,7 @@ class Activity < ActiveRecord::Base
   def deployed!
     update_attribute(:state, STATE_DEPLOYED)
   end
-    
+
   def to_s
     name
   end
