f1dbc249109ad7324abdaf8c0fcfc726c741dcfb
Joakim Bodin <joakim.bodin@gmail.com>
2009-02-23 12:16:52 +0100

    Fix bug with @change not being set when an error occures in the new change suggestion form.

--- app/controllers/change_suggestions_controller.rb
+++ app/controllers/change_suggestions_controller.rb
@@ -1,10 +1,11 @@
 class ChangeSuggestionsController < ApplicationController
   resource_controller
   belongs_to :activity
-  
+
   new_action.before { @change = object }
   new_action.wants.html { render :layout => false if request.xhr? }
-  
+
+  create.before { @change = object }
   create.success.wants.html do
     if request.xhr?
       render :partial => "changes/change", :collection => @activity.changes
@@ -19,20 +20,20 @@ class ChangeSuggestionsController < ApplicationController
       render :action => "new"
     end
   end
-  
+
   private
-    
+
   def build_object
     @object ||= Change.new(params[:change])
     @object.activity_id = params[:activity_id]
   end
-  
+
   private
-  
+
   def add_controller_crumbs
     add_app_controller_crumbs(parent_object.app)
     add_activities_controller_crumbs(parent_object.app, parent_object)
-    
+
     add_crumb 'Change Suggestions'
   end
 end
