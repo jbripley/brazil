2e9ae4a9d3bd0a991106ec369388dd6cdfe186b6
Joakim Bodin <joakim.bodin@gmail.com>
2009-02-23 16:07:45 +0100

    Return a partial error form when there is an failure in the partial app form.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,38 +1,43 @@
 class AppsController < ApplicationController
   resource_controller
-  
+
   index.before { @app = App.new }
   index.wants.atom
-  
+
   show.wants.html do
     if request.xhr?
       render :partial => 'app', :locals => {:app => @app}
     end
   end
-  
+
   create.wants.html do
     redirect_to app_activities_path(@app)
   end
-    
+
   edit.wants.html do
     if request.xhr?
       render :partial => 'edit_name', :locals => {:app => @app}
     end
   end
-  
-  update.wants.html do
+
+  update.success.wants.html do
     if request.xhr?
       render :partial => 'app', :locals => {:app => @app}
     else
       redirect_to app_activities_path(@app)
     end
   end
-  
+  update.failure.wants.html do
+    if request.xhr?
+      render :partial => 'edit_name', :locals => {:app => @app}
+    end
+  end
+
   private
-  
+
   def add_controller_crumbs
     add_crumb 'Apps', apps_path
-    
+
     if object
       add_crumb object.to_s, app_path(object)
     end
