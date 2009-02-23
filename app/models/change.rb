bb1754e7f8c1374b9146f4d00151637bc8c8b4e0
Joakim Bodin <joakim.bodin@gmail.com>
2009-02-23 10:08:20 +0100

    Only put saved and executed changes in suggested version SQL.

--- app/models/change.rb
+++ app/models/change.rb
@@ -2,18 +2,18 @@ class Change < ActiveRecord::Base
   STATE_SUGGESTED = 'suggested'
   STATE_SAVED = 'saved'
   STATE_EXECUTED = 'executed'
-  
+
   belongs_to :activity
-  
+
   validates_associated :activity
   validates_presence_of :sql
-  
+
   before_save :check_correct_state, :mark_activity_updated
-  
+
   def self.activity_sql(activity_id)
-    Change.find_all_by_activity_id(activity_id, :order => 'created_at ASC').collect {|change| change.sql}.join("\n")
+    Change.find_all_by_activity_id(activity_id, :order => 'created_at ASC', :conditions => {:state => [Change::STATE_EXECUTED, Change::STATE_SAVED]}).collect {|change| change.sql}.join("\n")
   end
-  
+
   def use_sql(sql, db_username, db_password)
     case state
     when STATE_EXECUTED
@@ -31,26 +31,26 @@ class Change < ActiveRecord::Base
     else
       raise Brazil::UnknowStateException, "Unknown state for Change when trying to execute SQL, #{self}"
     end
-    
+
     return true
   end
-  
+
   def to_s
     id.to_s
   end
-  
+
   def suggested?
     (state == STATE_SUGGESTED)
   end
-       
+
   def valid_email?(email)
     TMail::Address.parse(email) && /@/.match(email)
   rescue
     false
   end
-  
+
   validates_each :developer do |record, attr, value|
-    record.errors.add("#{attr} entry must be a valid email and") unless record.valid_email?(value)    
+    record.errors.add("#{attr} entry must be a valid email and") unless record.valid_email?(value)
   end
 
   validates_each :dba do |record, attr, value|
@@ -58,24 +58,24 @@ class Change < ActiveRecord::Base
       record.errors.add("#{attr} entry must be a valid email and")
     end
   end
-  
+
   private
-  
+
   def db_instance_dev
     activity.db_instances.each do |db_instance|
       return db_instance if db_instance.dev?
     end
-    
+
     raise Brazil::NoDBInstanceException, "#{activity} has no #{DbInstance::ENV_DEV} database instance set. Use Edit Activity to set one."
   end
-  
+
   def check_correct_state
     unless activity.development?
       errors.add_to_base("You can only add or update a change when its activity is in state development")
       false
     end
   end
-  
+
   def mark_activity_updated
     activity.update_attribute(:updated_at, Time.now)
   end
