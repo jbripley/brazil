c9bb863b77276775f53a0864737c4b03cc86ec02
Joakim Bodin <joakim.bodin@gmail.com>
2009-02-24 10:52:01 +0100

    Add vc_path to app inline edit form. Make ajax cancel button work like it should.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,25 +1,25 @@
 class AppsController < ApplicationController
   resource_controller
-  
+
   index.before { @app = App.new }
   index.wants.atom
-  
+
   show.wants.html do
     if request.xhr?
       render :partial => 'app', :locals => {:app => @app}
     end
   end
-  
+
   create.wants.html do
     redirect_to app_activities_path(@app)
   end
-    
+
   edit.wants.html do
     if request.xhr?
-      render :partial => 'edit_name', :locals => {:app => @app}
+      render :partial => 'edit_horizontal', :locals => {:app => @app}
     end
   end
-  
+
   update.wants.html do
     if request.xhr?
       render :partial => 'app', :locals => {:app => @app}
@@ -27,12 +27,12 @@ class AppsController < ApplicationController
       redirect_to app_activities_path(@app)
     end
   end
-  
+
   private
-  
+
   def add_controller_crumbs
     add_crumb 'Apps', apps_path
-    
+
     if object
       add_crumb object.to_s, app_path(object)
     end
