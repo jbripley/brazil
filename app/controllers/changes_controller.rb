3bc6ae04c0a7e883f5be8b031594737465753b1c
Joakim Bodin <joakim.bodin@gmail.com>
2009-03-06 17:00:42 +0100

    Remove dependency on resource_controller.

--- app/controllers/changes_controller.rb
+++ app/controllers/changes_controller.rb
@@ -1,12 +1,40 @@
 class ChangesController < ApplicationController
-  resource_controller
-  belongs_to :activity
+  # GET /apps/:app_id/activities/:activity_id/changes.xml
+  def index
+    @activity = Activity.find(params[:activity_id])
+    @changes = @activity.changes
+
+    respond_to do |format|
+      format.xml  { render :xml => @changes }
+    end
+  end
+
+  # GET /apps/:app_id/activities/:activity_id/changes/1.xml
+  def show
+    @activity = Activity.find(params[:activity_id])
+    @change = @activity.changes.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @change }
+    end
+  end
+
+  # GET /apps/:app_id/activities/:activity_id/changes/new.xml
+  def new
+    @activity = Activity.find(params[:activity_id])
+    @change = @activity.changes.build
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @change }
+    end
+  end
 
   # POST /apps/:app_id/activities/:activity_id/changes.format
   def create
     @activity = Activity.find(params[:activity_id])
-    @change = Change.new(params[:change])
-    @change.activity_id = params[:activity_id]
+    @change = @activity.changes.build(params[:change])
 
     if params[:create_change_execute_button]
       @change.state = Change::STATE_EXECUTED
@@ -40,13 +68,24 @@ class ChangesController < ApplicationController
     end
   end
 
-  edit.wants.html { render :layout => false if request.xhr? }
+  # GET /apps/:app_id/activities/:activity_id/changes/:change_id/edit
+  # GET /apps/:app_id/activities/:activity_id/changes/:change_id/edit.xml
+  def edit
+    @activity = Activity.find(params[:activity_id])
+    @change = @activity.changes.find(params[:id])
+
+    respond_to do |format|
+      format.html do # edit.html.erb
+        render :layout => false if request.xhr?
+      end
+      format.xml  { render :xml => @change }
+    end
+  end
 
   # PUT /apps/:app_id/activities/:activity_id/changes/:id.format
   def update
     @activity = Activity.find(params[:activity_id])
-    @change = Change.find(params[:id ])
-    @change.activity_id = params[:activity_id]
+    @change = @activity.changes.find(params[:id])
     @change.attributes = params[:change]
 
     if params[:edit_change_execute_submit]
@@ -84,13 +123,14 @@ class ChangesController < ApplicationController
   private
 
   def add_controller_crumbs
-    add_app_controller_crumbs(parent_object.app)
-    add_activities_controller_crumbs(parent_object.app, parent_object)
+    app = App.find(params[:app_id])
+    add_app_controller_crumbs(app)
+    add_activities_controller_crumbs(app, app.activities.find(params[:activity_id]))
 
     add_crumb 'Changes'
 
-    if object
-      add_crumb object.to_s
+    if params.has_key?(:id)
+      add_crumb app.activities.find(params[:activity_id]).changes.find(params[:id]).to_s
     end
   end
 end
