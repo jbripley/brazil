ce969edb6b93625136e8dc5e6210d29b9a05e7c3
Urban Edlund <urbane@ongame.com@ltsem0035.local>
2009-11-21 05:01:58 +0800

    Ensure that :vc_path path don't end with '/' when adding an app.
    Ending '/' will cause below error in apache.
    
    libsvn_subr/path.c:350: svn_path_remove_component: Assertion `is_canonical(path->data, path->len)' failed.

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,6 +2,7 @@ class App < ActiveRecord::Base
   has_many :activities, :order => 'updated_at DESC'
 
   validates_presence_of :name, :vc_path
+  validates_format_of :vc_path, :with => /^(\/[\w\d -]+)+$/
 
   def to_s
     name
